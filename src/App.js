16e0eac0e06e1c80210f3bdbe60318bfa7d2b723
komcal <kom.sujautra@gmail.com>
2017-08-30 08:38:38 +0700

    add input to todo list

--- src/App.js
+++ src/App.js
@@ -3,16 +3,53 @@ import './App.css';
 import Header from './containers/Header';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [],
+      text: '',
+    }
+  }
+  onInputChange = (e) => {
+    this.setState({
+      text: e.target.value,
+    });
+  }
+  addTodo = () => {
+    const text = this.state.text;
+    this.setState({
+      todos: [...this.state.todos, text],
+      text: '',
+    })
+  }
   render() {
     return (
       <div className="App">
         <Header />
         <div className="content">
-          <input
-            type="text"
-            name="todo"
-            className="todo-input"
-          />
+          <div className="input-wrapper">
+            <input
+              type="text"
+              name="todo"
+              className="todo-input"
+              onChange={this.onInputChange}
+              placeholder="type todo..."
+              value={this.state.text}
+            />
+            <button
+              onClick={this.addTodo}
+              className="todo-btn"
+            >
+              +
+            </button>
+          </div>
+          <div className="todo-list">
+            {
+              this.state.todos.map(todo => (
+                <div>{todo}</div>
+              ))
+            }
+          </div>
         </div>
       </div>
     );
